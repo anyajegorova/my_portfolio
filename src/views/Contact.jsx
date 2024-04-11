737dc1335763bb5ad74f278a97549adf5a63a72d
anyajegorova <anna.jerm@outlook.com>
2024-04-11 18:04:20 +0300

    edited styling and projects

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,8 +1,41 @@
 import './Contact.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+
 const Contact = () => {
+    const form = useRef();
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        emailjs
+            .sendForm(
+                'service_ivkah3m',
+                'template_fbfq4dm',
+                form.current,
+                'lMaEZmQbs-phf2SCI')
+            .then((result) => {
+                console.log(result.text);
+                form.current.reset();
+            },
+                (error) => {
+                    console.error(error.text);
+                }
+            )
+    }
     return (
-        <>
-        </>
+        <section className='contact_section'>
+            <form className='contact_form' ref={form} onSubmit={handleClick}>
+                <h2>Contact me now!</h2>
+                <label htmlFor="user_name">Name</label>
+                <input type='text' name='user_name' placeholder='Name' />
+                <label htmlFor="user_email">Email</label>
+                <input type='email' name='user_email' placeholder='Email' />
+                <label htmlFor="message">Message</label>
+                <textarea placeholder='Message' name='message'></textarea>
+                <button type='submit'>Send</button>
+            </form>
+
+        </section>
     )
 }
 
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,9 +1,72 @@
 import './Projects.css';
+import { useState } from 'react';
+import SayIt from '../assets/MacBookPro16_mockup_sayit.png';
+import GPT from '../assets/MacBookPro16_mockup_gpt.png';
+import SpaceX from '../assets/MacBookPro16_mockup_spaceX.png';
+import TastyBites from '../assets/MacBookPro16_mockup_tastybites.png';
+import CatStore from '../assets/MacBookPro16_mockup_catstore.png';
 
 const Projects = () => {
+
+    const projects = [
+        {
+            name: 'SayIt!',
+            image: SayIt,
+            description: 'Responsive full-stack MERN web application. Register, create a topic, say something and like what other users say.'
+        },
+        {
+            name: 'Another ChatGPT',
+            image: GPT,
+            description: 'A web application design of ChatGPT using OpenAI API, ReactJS, NodeJS and Express. Chat with AI and get responses in real-time.'
+        },
+        {
+            name: 'SpaceX Capsules',
+            image: SpaceX,
+            description: 'A web application using SpaceX API for mapping SpaceX capsules information into Ag-Grid-React table.'
+        },
+        {
+            name: 'TastyBites',
+            image: TastyBites,
+            description: 'A UI design for a recipe website using Figma'
+        },
+        {
+            name: 'CatStore',
+            image: CatStore,
+            description: 'A UI design for a cat online store using Figma.'
+        }
+    ]
+    const [currentProject, setCurrentProject] = useState(projects[0]);
+    const handleClick = (project) => {
+        setCurrentProject(project)
+    }
     return (
-        <>
-        </>
+        <section className='projects_section'>
+            <div className='navigation_wrapper'>
+                {projects.map((project, index) => (
+                    <div key={index} className='tab_container'>
+                        <h2
+                            className={currentProject?.name === project.name ? 'active' : ''}
+                            onClick={() => handleClick(project)}>
+                            {project.name}
+                        </h2>
+                    </div>
+                ))}
+            </div>
+            <div className='mockup_wrapper'>
+                {currentProject ?
+                    (<img src={currentProject.image} alt={currentProject.name}></img>)
+                    :
+                    (<img src={SayIt}></img>)
+                }
+            </div>
+            <div className='project_description_wrapper'>
+                {currentProject ?
+                    currentProject.description
+                    : projects[0].description
+                }
+
+            </div>
+        </section>
     )
 }
 
