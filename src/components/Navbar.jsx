47cde8f2a460d8146623fde515b50c98e9a75775
anyajegorova <anna.jerm@outlook.com>
2024-04-11 10:52:23 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className='navigation'>
+            <h3>Contact</h3>
+        </nav>
+    )
+}
+
+export default Navbar;
