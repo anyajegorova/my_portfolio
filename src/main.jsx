9f079a0f2ef544ffee75d99e046c44caad13ef19
anyajegorova <anna.jerm@outlook.com>
2024-04-14 16:26:18 +0300

    removing overscroll none

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-document.body.addEventListener('touchmove', function (event) {
-  event.preventDefault();
-}, { passive: false });
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
