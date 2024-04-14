f9d39be8a5bd229f03d89caf7f590dadb9b083e3
anyajegorova <anna.jerm@outlook.com>
2024-04-14 22:01:49 +0300

    fixing fonts and home page transitions

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,29 +1,46 @@
 import './About.css';
+import { useState, useEffect } from 'react';
 
 const About = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
     return (
         <section className='about_section'>
             <div className='title_wrapper'>
                 <h1>Anna Jegorova</h1>
                 <h2>Software Developer</h2>
             </div>
-            <div className='info_wrapper'>
-                <h4>
-                    I utilize the best design practices
-                    for shaping holistic and modern interfaces
-                </h4>
-                <h4>
-                    .. and turn them into code using latest
-                    technologies
-                </h4>
-            </div>
-            <div className='technologies_wrapper'>
-                <h3>ReactJS</h3>
-                <h3>NodeJS</h3>
-                <h3>Express</h3>
-                <h3>JavaScript</h3>
-                <h3>MongoDB</h3>
-                <h3>PostgreSQL</h3>
+            <div className='all_info_container'>
+
+
+                <div className='info_wrapper'>
+                    <h4 className={`${isVisible ? 'visible' : ''}`}>
+                        I utilize the best design practices
+                        for shaping holistic and modern interfaces
+                    </h4>
+                    <h4 className={`${isVisible ? 'visible' : ''}`}>
+                        .. and turn them into code using latest
+                        technologies
+                    </h4>
+                </div>
+                <div className='technologies_container'>
+
+                    <span className={`${isVisible ? 'visible' : ''}`}> &#123; </span>
+                    <div className='technologies_wrapper'>
+                        <h3 className={`${isVisible ? 'visible' : ''}`}>ReactJS</h3>
+                        <h3 className={`${isVisible ? 'visible' : ''}`}>NodeJS</h3>
+                        <h3 className={`${isVisible ? 'visible' : ''}`}>Express</h3>
+                        <h3 className={`${isVisible ? 'visible' : ''}`}>JavaScript</h3>
+                        <h3 className={`${isVisible ? 'visible' : ''}`}>MongoDB</h3>
+                        <h3 className={`${isVisible ? 'visible' : ''}`}>PostgreSQL</h3>
+                    </div>
+                    <span className={`${isVisible ? 'visible' : ''}`}> &#125; </span>
+                </div>
+
             </div>
         </section>
     )
