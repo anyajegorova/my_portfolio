c0c7794f30f315bc9fe33e7d95e730234cf78745
anyajegorova <anna.jerm@outlook.com>
2025-02-28 10:35:21 +0200

    re-design: fixed button animation, adjusted project card transtion on hover

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -9,8 +9,8 @@ const CustomButton: React.FC<CustomButtonProps> = ({ text }) => {
     return (
         <button className='custom_button' style={{ borderRadius: '16px' }}>
             <svg width="160px" height="40px" viewBox="0 0 160 40" className="border">
-                <rect x="1.1" y="0.5" width="159.2" height="38" rx="16" ry="16" className="bg-line" />
-                <rect x="1.1" y="0.5" width="159.2" height="38" rx="16" ry="16" className="hl-line" />
+                <rect x="-0.5" y="-1" width="159.2" height="38" rx="16" ry="16" className="bg-line" />
+                <rect x="-0.5" y="-1" width="159.2" height="38" rx="16" ry="16" className="hl-line" />
             </svg>
             <span>{text}</span>
         </button>
